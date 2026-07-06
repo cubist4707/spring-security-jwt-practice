3ed77cb66cbb66ad8decff72fb41723a2a5c79cc
Cubist4707 <cubist4707@gmail.com>
2026-07-06 12:55:41 +0900

    feat: JWT 기반 로그아웃 및 Redis 블랙리스트 기능 구현

--- src/main/java/org/example/signup/security/JwtFilter.java
+++ src/main/java/org/example/signup/security/JwtFilter.java
@@ -5,9 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,15 +19,20 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String token = resolveToken(request);
         if (token != null && jwtUtil.validateToken(token)) {
-            String username = jwtUtil.getUserInfoFromToken(token);
-            Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, null);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String isLogout = (String) redisTemplate.opsForValue().get(token);
+
+            if (ObjectUtils.isEmpty(isLogout)) {
+                String username = jwtUtil.getUserInfoFromToken(token);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
 
         filterChain.doFilter(request, response);
